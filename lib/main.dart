1092449a24eb4593eebf41c67da66cb5c9aed453
Versy <sohnsmith@sonjaehyeong-ui-MacBookPro.local>
2023-05-01 16:59:21 +0900

    init

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:vreal_0/src/app.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme:
+          ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink)),
+      title: 'Vreal',
+      home: const App(),
+    );
+  }
+}
---
+++ lib/src/app.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'vreal',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            ElevatedButton(
+                onPressed: () {
+                  debugPrint('hello1');
+                },
+                child: const Text(
+                  '블랙마카',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                )),
+            ElevatedButton(
+                onPressed: () {
+                  debugPrint('hello2');
+                },
+                child: const Text(
+                  '아르기닌',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                )),
+            ElevatedButton(
+                onPressed: () {
+                  debugPrint('hello3');
+                },
+                child: const Text(
+                  '쏘팔메토',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
