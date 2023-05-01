5c126b1852cd3fa1dbfc71f183071893aecf61ae
Versy <sohnsmith@sonjaehyeong-ui-MacBookPro.local>
2023-05-01 20:21:44 +0900

    middle

--- lib/main.dart
+++ lib/main.dart
@@ -9,14 +9,13 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme:
           ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink)),
-      title: 'Vreal',
-      home: const App(),
+      title: 'vreal',
+      home: App(),
     );
   }
 }
--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class App extends StatelessWidget {
-  const App({super.key});
+  final List<String> medicineList = ['블랙마카', '아르기닌', '소팔메토', '아연'];
+
+  App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -9,39 +11,45 @@ class App extends StatelessWidget {
       appBar: AppBar(
         title: const Text(
           'vreal',
-          style: TextStyle(fontWeight: FontWeight.bold),
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
         ),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            ElevatedButton(
-                onPressed: () {
-                  debugPrint('hello1');
+            Center(
+                child: Text(
+              '영양제 비교',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+            )),
+            Expanded(
+              child: ListView.separated(
+                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                itemCount: medicineList.length,
+                separatorBuilder: (context, index) {
+                  return SizedBox(height: 30);
                 },
-                child: const Text(
-                  '블랙마카_수정됨',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                )),
-            ElevatedButton(
-                onPressed: () {
-                  debugPrint('hello2');
+                itemBuilder: (context, index) {
+                  return ElevatedButton(
+                      style: ButtonStyle(
+                          padding: MaterialStateProperty.all(
+                              EdgeInsets.symmetric(vertical: 10))),
+                      onPressed: () {
+                        debugPrint('${medicineList[index]}');
+                      },
+                      child: Text(
+                        '${medicineList[index]}',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ));
                 },
-                child: const Text(
-                  '아르기닌',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                )),
-            ElevatedButton(
-                onPressed: () {
-                  debugPrint('hello3');
-                },
-                child: const Text(
-                  '쏘팔메토',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                ))
+              ),
+            )
           ],
         ),
       ),
