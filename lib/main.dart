010de395ee403b764ebc52afb08f8472784be717
Versy <sohnsmith@sonjaehyeong-ui-MacBookPro.local>
2023-05-05 22:52:32 +0900

    white

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      color: Colors.white,
       theme: ThemeData(
         appBarTheme:
             const AppBarTheme(backgroundColor: Colors.white, elevation: 0),
